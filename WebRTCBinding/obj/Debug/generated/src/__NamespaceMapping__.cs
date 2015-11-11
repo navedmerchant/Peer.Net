c82e05ae3c3269b081127fea899cd42d75995bdf
akonsand <naved.merchant@gmail.com>
2015-11-11 14:09:28 -0500

    Removed duplicate Project
    A duplicate of WebRTC bindings was was also pushed to the server

--- WebRTCBinding/obj/Debug/generated/src/__NamespaceMapping__.cs
+++
@@ -1,4 +0,0 @@
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "me.kevingleason.pnwebrtc", Managed="ME.Kevingleason.Pnwebrtc")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.webrtc", Managed="Org.Webrtc")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.webrtc.videoengine", Managed="Org.Webrtc.Videoengine")]
-[assembly:global::Android.Runtime.NamespaceMapping (Java = "org.webrtc.voiceengine", Managed="Org.Webrtc.Voiceengine")]
